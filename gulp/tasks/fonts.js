2e5db4fd318f3e5d4cc5a2d6d5847dae22d88fc3
Yaroslav Kobets <yaroslavkobetsV@gmail.com>
2023-09-06 23:14:50 +0300

    fix: pt bug

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,6 +1,6 @@
-import fs from 'fs';
-import fonter from 'gulp-fonter-fix';
-import ttf2woff2 from 'gulp-ttf2woff2';
+import fs from 'fs'
+import fonter from 'gulp-fonter-fix'
+import ttf2woff2 from 'gulp-ttf2woff2'
 
 export const otfToTtf = () => {
 	return app.gulp
@@ -10,16 +10,16 @@ export const otfToTtf = () => {
 				app.plugins.notify.onError({
 					title: 'FONTS',
 					message: 'Error: <%= error.message %>',
-				})
-			)
+				}),
+			),
 		)
 		.pipe(
 			fonter({
 				formats: ['ttf'],
-			})
+			}),
 		)
-		.pipe(app.gulp.dest(`${app.path.srcFolder}/fonts/`));
-};
+		.pipe(app.gulp.dest(`${app.path.srcFolder}/fonts/`))
+}
 export const ttfToWoff = () => {
 	return app.gulp
 		.src(`${app.path.srcFolder}/fonts/*.ttf`, {})
@@ -28,75 +28,75 @@ export const ttfToWoff = () => {
 				app.plugins.notify.onError({
 					title: 'FONTS',
 					message: 'Error: <%= error.message %>',
-				})
-			)
+				}),
+			),
 		)
 		.pipe(
 			fonter({
 				formats: ['woff'],
-			})
+			}),
 		)
 		.pipe(app.gulp.dest(`${app.path.build.fonts}`))
 		.pipe(app.gulp.src(`${app.path.srcFolder}/fonts/*.ttf`))
 		.pipe(ttf2woff2())
 		.pipe(app.gulp.dest(`${app.path.build.fonts}`))
 		.pipe(app.gulp.src(`${app.path.srcFolder}/fonts/*.{woff,woff2}`))
-		.pipe(app.gulp.dest(`${app.path.build.fonts}`));
-};
+		.pipe(app.gulp.dest(`${app.path.build.fonts}`))
+}
 export const fonstStyle = () => {
-	let fontsFile = `${app.path.srcFolder}/scss/fonts/fonts.scss`;
-	app.isFontsReW ? fs.unlink(fontsFile, cb) : null;
+	let fontsFile = `${app.path.srcFolder}/scss/fonts/fonts.scss`
+	app.isFontsReW ? fs.unlink(fontsFile, cb) : null
 	fs.readdir(app.path.build.fonts, function (err, fontsFiles) {
 		if (fontsFiles) {
 			if (!fs.existsSync(fontsFile)) {
-				fs.writeFile(fontsFile, '', cb);
-				let newFileOnly;
+				fs.writeFile(fontsFile, '', cb)
+				let newFileOnly
 				for (var i = 0; i < fontsFiles.length; i++) {
-					let fontFileName = fontsFiles[i].split('.')[0];
+					let fontFileName = fontsFiles[i].split('.')[0]
 					if (newFileOnly !== fontFileName) {
 						let fontName = fontFileName.split('-')[0]
 							? fontFileName.split('-')[0]
-							: fontFileName;
+							: fontFileName
 						let fontWeight = fontFileName.split('-')[1]
 							? fontFileName.split('-')[1]
-							: fontFileName;
+							: fontFileName
 						if (fontWeight.toLowerCase() === 'thin') {
-							fontWeight = 100;
+							fontWeight = 100
 						} else if (fontWeight.toLowerCase() === 'extralight') {
-							fontWeight = 200;
+							fontWeight = 200
 						} else if (fontWeight.toLowerCase() === 'light') {
-							fontWeight = 300;
+							fontWeight = 300
 						} else if (fontWeight.toLowerCase() === 'medium') {
-							fontWeight = 500;
+							fontWeight = 500
 						} else if (fontWeight.toLowerCase() === 'semibold') {
-							fontWeight = 600;
+							fontWeight = 600
 						} else if (fontWeight.toLowerCase() === 'bold') {
-							fontWeight = 700;
+							fontWeight = 700
 						} else if (
 							fontWeight.toLowerCase() === 'extrabold' ||
 							fontWeight.toLowerCase() === 'heavy'
 						) {
-							fontWeight = 800;
+							fontWeight = 800
 						} else if (fontWeight.toLowerCase() === 'black') {
-							fontWeight = 900;
+							fontWeight = 900
 						} else {
-							fontWeight = 400;
+							fontWeight = 400
 						}
 						fs.appendFile(
 							fontsFile,
-							`@font-face {\n\tfont-family: ${fontName};\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`,
-							cb
-						);
-						newFileOnly = fontFileName;
+							`@font-face {\n\tfont-family: ${fontName};\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\n`,
+							cb,
+						)
+						newFileOnly = fontFileName
 					}
 				}
 			} else {
-				console.log('Файл scss/fonts/fonts.scss вже існує. ');
+				console.log('Файл scss/fonts/fonts.scss вже існує. ')
 			}
 		} else {
-			fs.unlink(fontsFile, cb);
+			fs.unlink(fontsFile, cb)
 		}
-	});
-	return app.gulp.src(`${app.path.srcFolder}`);
-};
+	})
+	return app.gulp.src(`${app.path.srcFolder}`)
+}
 function cb() {}
